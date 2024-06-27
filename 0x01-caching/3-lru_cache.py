3d20385829924dcc11503c218abb3d06fca682ce
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-27 05:42:01 +0300

    fuck this project i  give up

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,27 +9,22 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """init function"""
         super().__init__()
-        self.use_queue = {}
+        self.use_queue = []
 
     def put(self, key, item):
         """Assign item to given key in cache"""
         if (key is not None and item is not None):
             if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                least_used = list(self.use_queue.keys())[0]
-                del self.use_queue[least_used]
-                if (key not in self.cache_data):
-                    del self.cache_data[least_used]
-                    print(f"DISCARD: {least_used}")
+                least_used = self.use_queue.pop(0)
+                self.cache_data.pop(least_used)
+                print(f"DISCARD: {least_used}")
             self.cache_data[key] = item
-            if key in self.use_queue:
-                del self.use_queue[key]
-            self.use_queue[key] = ""
+            self.use_queue.append(key)
 
     def get(self, key):
         """Retrieve item through given key from cache"""
         if (key is None or key not in self.cache_data):
             return None
-        if key in self.use_queue:
-            del self.use_queue[key]
-        self.use_queue[key] = ""
+        self.use_queue.remove(key)
+        self.use_queue.append(key)
         return self.cache_data[key]
