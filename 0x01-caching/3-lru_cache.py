146cd8e9f50cb53387292ab3b99f455501743eec
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-27 05:53:02 +0300

    wtf

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Task 3: LRU Caching"""
+""" doc doc doc """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
     """LRU Class"""
     def __init__(self):
-        """init function"""
+        """doc doc doc"""
         super().__init__()
-        self.use_queue = []
+        self.order = []
 
     def put(self, key, item):
-        """Assign item to given key in cache"""
+        """doc doc doc"""
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                removed = self.use_queue.pop(0)
+                removed = self.order.pop(0)
                 self.cache_data.pop(removed)
                 print("DISCARD: {}".format(removed))
             self.cache_data[key] = item
-            self.use_queue.append(key)
+            self.order.append(key)
 
     def get(self, key):
-        """Retrieve item through given key from cache"""
+        """doc doc doc"""
         if key in self.cache_data:
-            self.use_queue.remove(key)
-            self.use_queue.append(key)
+            self.order.remove(key)
+            self.order.append(key)
             return self.cache_data.get(key)
