43756037f4d2bae3ffc90054f85541abbf60cf48
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-27 05:42:52 +0300

    brug

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""Task 3: LRU Caching"""
-
-BaseCaching = __import__('base_caching').BaseCaching
+""" doc doc doc """
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRU Class"""
+    """doc doc doc"""
+
     def __init__(self):
-        """init function"""
+        """doc doc doc"""
         super().__init__()
-        self.use_queue = []
+        self.order = []
 
     def put(self, key, item):
-        """Assign item to given key in cache"""
-        if (key is not None and item is not None):
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                least_used = self.use_queue.pop(0)
-                self.cache_data.pop(least_used)
-                print(f"DISCARD: {least_used}")
+        """doc doc doc"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop(0)
+                self.cache_data.pop(removed)
+                print("DISCARD: {}".format(removed))
             self.cache_data[key] = item
-            self.use_queue.append(key)
+            self.order.append(key)
 
     def get(self, key):
-        """Retrieve item through given key from cache"""
-        if (key is None or key not in self.cache_data):
-            return None
-        self.use_queue.remove(key)
-        self.use_queue.append(key)
-        return self.cache_data[key]
+        """doc doc doc"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
