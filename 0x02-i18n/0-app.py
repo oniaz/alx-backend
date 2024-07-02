42c9882067547c33389cba4234dfb95ce668dd53
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-03 00:12:00 +0300

    module doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+"""1. Basic Babel setup"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
