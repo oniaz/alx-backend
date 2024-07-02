0a30347004e6a5747fcd93d890a1e1b0457308e2
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-03 01:17:36 +0300

    pep8

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,20 +9,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     """
     Configuration class for setting up available languages and default
     locale/timezone for the Flask app
     """
-    LANGUAGES =["en", "fr"]
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def home():
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
