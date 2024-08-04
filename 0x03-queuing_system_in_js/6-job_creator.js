cd425e00bd2928f82f3a718fb3fe64784a028e91
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-04 03:59:35 +0300

    6. Create the Job creator - kue queue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const jobData = {
+  phoneNumber: "1234567890",
+  message: 'whazzzzzuuuuuuhp',
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(` Notification job created: ${job.id}`);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
